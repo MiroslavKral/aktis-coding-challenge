80c01ffa682a2522408a3ed82edf61e1d32472d6
Miroslav Král <miroslav.kral.86@gmail.com>
2019-07-16 11:58:25 +0200

    Running Median - refactoring

--- src/running-median.ts
+++ src/running-median.ts
@@ -3,41 +3,48 @@ const formatNumber = (value: number): string => {
   return rounded.toFixed(1);
 };
 
-export const runningMedian = (numberOfValues: number, values: number[]) => {
-  const number = 1;
-  console.log('start');
-  console.log('TO FIXED: ', number.toFixed(1));
+const isEven = (n: number) => n % 2 === 0;
+
+const computeMedianForOddLength = (values: number[]): string => {
+  const index = Math.round(values.length / 2) - 1;
+  // console.log('ODD LENGTH: ', values.length);
+  // console.log('ODD index: ', index);
+  return formatNumber(values[index]);
+};
+
+const computeMedianForEvenLength = (values: number[]): string => {
+  const higherIndex = values.length / 2;
+  const lowerIndex = higherIndex - 1;
+  // console.log('EVEN LENGTH: ', values.length);
+  // console.log('EVEN LI: ', lowerIndex);
+  // console.log('EVEN HI: ', higherIndex);
+  const firstValue = values[lowerIndex];
+  const secondValue = values[higherIndex];
+  // console.log('EVEN first: ', firstValue);
+  // console.log('EVEN second: ', secondValue);
+  const median = (firstValue + secondValue) / 2;
+  return formatNumber(median);
+};
+
+const computeMedian = (values: number[]) => {
+  return isEven(values.length)
+    ? computeMedianForEvenLength(values)
+    : computeMedianForOddLength(values);
+};
+
+export const runningMedian = (
+  numberOfValues: number,
+  values: string[]
+): string[] => {
   const sortedArray: number[] = [];
   const results: string[] = [];
 
   values.forEach(value => {
-    sortedArray.push(value);
+    sortedArray.push(parseInt(value));
 
-    if (sortedArray.length === 1) {
-      results.push(formatNumber(sortedArray[0]));
-    } else if (sortedArray.length === 2) {
-      const median = (sortedArray[0] + sortedArray[1]) / 2;
-      results.push(formatNumber(median));
-    } else if (sortedArray.length % 2 === 0) {
-      const higherIndex = sortedArray.length / 2;
-      const lowerIndex = higherIndex - 1;
-      console.log('M2 LENGTH: ', sortedArray.length);
-      console.log('M2 LI: ', lowerIndex);
-      console.log('M2 HI: ', higherIndex);
-      const firstValue = sortedArray[lowerIndex];
-      const secondValue = sortedArray[higherIndex];
-      console.log('M2 first: ', firstValue);
-      console.log('M2 second: ', secondValue);
-      const median = (firstValue + secondValue) / 2;
-      results.push(formatNumber(median));
-      console.log('M2: ', results);
-    } else if (sortedArray.length % 2 !== 0) {
-      const index = Math.round(sortedArray.length / 2) - 1;
-      console.log('M3 LENGTH: ', sortedArray.length);
-      console.log('M3 index: ', index);
-      results.push(formatNumber(sortedArray[index]));
-      console.log('M3: ', results);
-    }
+    const median = computeMedian(sortedArray);
+    console.log(median);
+    results.push(median);
   });
 
   return results;
